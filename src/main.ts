61cd97ac476fcb36b3797e23ce8edf0dc25a557e
neutrino84 <neutrino84@gmail.com>
2020-05-21 16:57:42 -0700

    removed exports

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,2 @@
 import Core from './core'
-
-import Module from './core/modules/Module'
-import ModuleRegistry from './core/ModuleRegistry'
-
-import GridManager from './core/modules/grid/GridManager'
-import ColumnManager from './core/modules/column/ColumnManager'
-import RowManager from './core/modules/grid/GridManager'
-
-export default {
-  Core,
-
-  ModuleRegistry,
-  Module,
-
-  GridManager,
-  ColumnManager,
-  RowManager,
-}
+export default Core
