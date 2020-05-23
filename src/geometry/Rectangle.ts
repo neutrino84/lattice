d728d9bc091e9dc400555dae8f724ab6dc4261ee
neutrino84 <neutrino84@gmail.com>
2020-05-22 17:52:31 -0700

    added rectangle geometry class to hold bounding boxes

---
+++ src/geometry/Rectangle.ts
@@ -0,0 +1,22 @@
+export default class Rectangle extends DOMRect {
+  constructor(rect: DOMRect = new DOMRect()) {
+    super(rect.x, rect.y, rect.width, rect.height)
+  }
+
+  extend(rect: Rectangle): void {
+		if (rect.x+rect.width > this.x+this.width) {
+      this.width = rect.x+rect.width-this.x;
+    }
+		if (rect.y+rect.height > this.y+this.height) {
+      this.height = rect.y+rect.height-this.y;
+    }
+		if (rect.x < this.x) {
+      this.width += this.x-rect.x;
+      this.x = rect.x;
+    }
+		if (rect.y < this.y) {
+      this.height += this.y-rect.y;
+      this.y = rect.y;
+    }
+  }
+}
