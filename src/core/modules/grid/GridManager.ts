3409d4c9b7a22a20fc6f29237eb8ac30fc4d5f3e
neutrino84 <neutrino84@gmail.com>
2020-05-24 08:52:41 -0700

    added root component layout management logic

--- src/core/modules/grid/GridManager.ts
+++ src/core/modules/grid/GridManager.ts
@@ -4,12 +4,38 @@ import Module from '../Module'
 export default class GridManager extends Module {
   constructor(core: Core) {
     super(core)
+
+    // subscribe listeners
+    this.core.on('column:nodes:created', this.resize, this)
   }
 
+  /*
+   *
+   */
   public init(): void {
     super.init()
   }
 
+  /*
+   *
+   */
+  public resize(): void {
+    let core = this.core
+    let rrect = core.root.getBoundingClientRect()
+    let hrect = core.header.getBoundingRectangle()
+    let frect = core.footer.getBoundingRectangle()
+    let height = rrect.height - frect.height - hrect.height
+
+    core.grid.attributes({
+      style: {
+        height: height + 'px'
+      }
+    })
+  }
+
+  /*
+   *
+   */
   public destroy(): void {
     super.destroy()
   }
